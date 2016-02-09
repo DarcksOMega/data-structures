525cbd6d3a76ccc7542d03d13e63ce53fc9184fb
AULA1 <AULA1@192.168.31.7>
2016-02-09 16:12:49 -0500

    launch

--- src/unity0/Test.java
+++ src/unity0/Test.java
@@ -13,7 +13,19 @@ public class Test {
         s.add( 20 );
         s.add( 30 );
         
-        s.print( ); // 10, 20, 30
+        s.print( ); // Imprimira estos números: 10, 20, 30
+        
+        s.remove(); /*Como es una pila, se remueve el ultimo que fue adicionado, en este caso, el
+        nùmero 30 */
+        
+        s.print(); /* Aquí imprime los 2 primeros números, que son el 10 y el 20*/
+        
+        for (int i=2; i<20; i+=2){ //Este es un incremento de 2 en 2
+            s.add(i);
+        }
+        
+        s.print(); //Se imprimiran los elementos adicionales
+        
     }
     
 }
