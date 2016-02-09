5d50b0002fd1f31c2ea23fc5459dc9a0cfa61bf4
AULA1 <AULA1@192.168.31.7>
2016-02-09 17:04:16 -0500

    launch

--- src/unity0/Queue.java
+++ src/unity0/Queue.java
@@ -5,44 +5,44 @@ package unity0;
  * @author Darcks OMega
  */
 public class Queue {
-    
+
     private int[] data;
     private int top;
-    
-    public Queue(){
-        
-        data = new int [100];
+
+    public Queue() {
+
+        data = new int[100];
         top = 0;
-        
+
     }
-    
-    public void add (int d){
-        
-        data [top]= d;
+
+    public void add(int d) {
+
+        data[top] = d;
         top++;
-        
+
     }
-    
-    public int remove(){
-        
-        int d= data[0]; //La primera posicion esta en data y en el lugar 0.
-        for (int i = 0; i< top -1; i++){
-            data [i] = data [i+1]; /*En data sub i, se va a compiar lo que esta en
-            data sub i+1.
-            */
+
+    public int remove() {
+
+        int d = data[0]; //La primera posicion esta en data y en el lugar 0.
+        for (int i = 0; i < top - 1; i++) {
+            data[i] = data[i + 1]; /*En data sub i, se va a compiar lo que esta en
+             data sub i+1.
+             */
+
         }
         top--;
         return d;
-        
+
     }
-    
-    public void print(){
-        
-        for(int i= 0; i<top; i++){
-            System.out.print(" "+data [i]);
+
+    public void print() {
+
+        for (int i = 0; i < top; i++) {
+            System.out.print(" " + data[i]);
         }
         System.out.println();
-        }
-        
     }
-    
+
+}
--- src/unity0/Test.java
+++ src/unity0/Test.java
@@ -5,45 +5,45 @@ package unity0;
  * @author Darcks OMega
  */
 public class Test {
-    
-    public static void main (String [] args){
-        
+
+    public static void main(String[] args) {
+
         TestingStacks();
         TestingQueue();
-        
+
     }
-    
-    public static void TestingStacks(){
-    
+
+    public static void TestingStacks() {
+
         System.out.println(" Stack Testing ");
         Stack s = new Stack();
-        s.add( 10 );
-        s.add( 20 );
-        s.add( 30 );
-        
-        s.print( ); // Imprimira estos números: 10, 20, 30
-        
+        s.add(10);
+        s.add(20);
+        s.add(30);
+
+        s.print(); // Imprimira estos números: 10, 20, 30
+
         s.remove(); /*Como es una pila, se remueve el ultimo que fue adicionado, en este caso, el
-        nùmero 30 */
-        
+         nùmero 30 */
+
         s.print(); // Aquí imprime los 2 primeros números, que son el 10 y el 20
-        
-        for (int i=2; i<20; i+=2){ //Este es un incremento de 2 en 2
+
+        for (int i = 2; i < 20; i += 2) { //Este es un incremento de 2 en 2
             s.add(i);
         }
-        
+
         s.print(); //Se imprimiran los elementos adicionales
     }
-    
-    public static void TestingQueue(){
-        
+
+    public static void TestingQueue() {
+
         System.out.println(" Queue Testing ");
-        Queue q= new Queue();
-        for (int i=1; i<= 20; i++){
+        Queue q = new Queue();
+        for (int i = 1; i <= 20; i++) {
             q.add(i);
         }
         System.out.print(" Initial value: ");
         q.print();
-        
+
     }
 }
